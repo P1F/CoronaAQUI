523c1fbfa011e84aa3f6baa5aeb736c93d991c3e
rodfrigol <rodrigoalalmeida@gmail.com>
2020-07-17 03:17:38 -0300

    implementação header

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\rodri\onedrive\documentos\ita\ces22\avaliações\django\coronaaqui\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
