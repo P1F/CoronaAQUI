5b9564e3a231255d6f6771be5dffb96b178d44dc
P1F <rtsouza15@gmail.com>
2020-07-17 00:50:26 -0400

    map view implemented

--- home/views.py
+++ home/views.py
@@ -3,6 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-# Create your views here.
+    return render(request, 'index.html')
